8506d945bca654185d8e0876f5c3c6e164d2d6e6
Olawoyin Joseph Folasakin <olawoyinjoseph05@gmail.com>
2026-02-24 18:55:16 +0100

    Background change

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// alert("Working")
+const title = document.getElementById("title")
+const description = document.getElementById("description")
+
+const addSchedule = () => {
+
+    console.log(title.value);
+    console.log(description.value);
+    
+}
